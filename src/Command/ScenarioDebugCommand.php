b7f62ce7e5c1b728681453ba2c23b4cc5dbd5bd1
Christina Koenig <christina.koenig@looriva.de>
2026-03-06 21:30:42 +0100

    implemented as command

--- src/Command/ScenarioDebugCommand.php
+++ src/Command/ScenarioDebugCommand.php
@@ -11,9 +11,16 @@
 
 namespace Scenario\Symfony\Command;
 
+use Scenario\Core\Application;
+use Scenario\Core\Contract\CliOutput;
+use Scenario\Core\PHPUnit\Finder\ScenarioTestFinder;
+use Scenario\Core\Runtime\ScenarioDefinition;
+use Scenario\Core\Runtime\ScenarioRegistry;
+use Scenario\Symfony\Console\Output;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process;
 
 final class ScenarioDebugCommand extends ScenarioCommand
@@ -28,10 +35,120 @@ final class ScenarioDebugCommand extends ScenarioCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $style = new Output(new SymfonyStyle($input, $output));
+        (new Application())->prepare();
+
+        $scenarioDefinitions = ScenarioRegistry::getInstance()->all();
+        $testClasses = (new ScenarioTestFinder())->all();
+
+        $type = $this->getSelectedType($style, $scenarioDefinitions, $testClasses);
+        if ($type === false) {
+            $style->error('No scenarios or unit tests were found, please create one.');
+            return Command::FAILURE;
+        }
+
+        return match($type) {
+            'Scenario' => $this->debugScenario($scenarioDefinitions, $output, $style),
+            'Unit Test' => $this->debugTest($testClasses, $output, $style),
+        };
+    }
+
+    /**
+     * @param array<class-string|string, ScenarioDefinition> $scenarioDefinitions
+     * @param array<class-string, list<non-empty-string>> $testClasses
+     * @return 'Scenario'|'Unit Test'|false
+     */
+    private function getSelectedType(CliOutput $style, array $scenarioDefinitions, array $testClasses): string|false
+    {
+        if (count($scenarioDefinitions) === 0
+            && count($testClasses) === 0) {
+            return false;
+        }
+
+        if (count($scenarioDefinitions) === 0) {
+            return 'Unit Test';
+        }
+
+        if (count($testClasses) === 0) {
+            return 'Scenario';
+        }
+
+        /** @var 'Scenario'|'Unit Test' $selected */
+        $selected = $style->choice('Which kind of class would you like to debug?', ['Scenario', 'Unit Test']);
+        return $selected;
+    }
+
+    /**
+     * @param array<class-string|string, ScenarioDefinition> $scenarioDefinitions
+     */
+    private function debugScenario(array $scenarioDefinitions, OutputInterface $output, CliOutput $style): int
+    {
+        $scenarios = [];
+        foreach ($scenarioDefinitions as $scenarioDefinition) {
+            $scenarios[$scenarioDefinition->class . ' (' . $scenarioDefinition->suite . ')'] = $scenarioDefinition;
+        }
+
+        /** @var list<non-falsy-string> $options */
+        $options = array_values(array_unique(array_keys($scenarios)));
+        $choosen = $style->choice('Which scenario would you like to debug?', $options);
+
+        /** @var class-string $scenarioClass */
+        $scenarioClass = $scenarios[$choosen]->class;
+        $this->runDebugClass($output, $scenarioClass, null);
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @param array<class-string, list<non-empty-string>> $classesMethods
+     */
+    private function debugTest(array $classesMethods, OutputInterface $output, CliOutput $style): int
+    {
+        /** @var list<class-string> $testClasses */
+        $testClasses = array_keys($classesMethods);
+
+        if (count($testClasses) === 1) {
+            $testClass = array_shift($testClasses);
+        } else {
+            /** @var class-string $testClass */
+            $testClass = $style->choice('Which class would you like to debug?', $testClasses);
+        }
+
+        /** @var list<non-empty-string> $methods */
+        $methods = $classesMethods[$testClass];
+
+        if (count($methods) === 0) {
+            $this->runDebugClass($output, $testClass, null);
+            return Command::SUCCESS;
+        }
+
+        if (count($methods) === 1) {
+            $method = array_shift($methods);
+        } else {
+            $method = $style->choice(sprintf('Which method would you like to debug from %s?', $testClass), $methods);
+        }
+
+        $this->runDebugClass($output, $testClass, $method);
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @param class-string $testClass
+     */
+    private function runDebugClass(OutputInterface $output, string $testClass, ?string $method): bool
+    {
+        $arguments = [
+            $testClass,
+        ];
+        if ($method !== null) {
+            $arguments[] = $method;
+        }
+
         $process = new Process([
             PHP_BINARY,
             $this->getCliPath(),
             'debug',
+            ...$arguments,
             '--force',
             '--quiet',
         ], $this->getKernel()->getProjectDir());
@@ -42,8 +159,6 @@ final class ScenarioDebugCommand extends ScenarioCommand
             $output->write($buffer);
         });
 
-        return $process->isSuccessful() === true
-            ? Command::SUCCESS
-            : Command::FAILURE;
+        return $process->isSuccessful();
     }
 }
